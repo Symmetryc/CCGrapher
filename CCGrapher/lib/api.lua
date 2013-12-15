ee19055f567fa06006604d32f8a8baa53b35c17f
Wobbo <rene.mellema@gmail.com>
2013-12-15 15:37:24 +0100

    Added return and made a table

--- CCGrapher/lib/api.lua
+++ CCGrapher/lib/api.lua
@@ -1,3 +1,4 @@
+return {
 ---------------------
 -- Table functions --
 ---------------------
@@ -6,20 +7,20 @@
 -- This functions creates a deep copy of _t
 -- @param _t The table to be copied
 -- @return A deep copy of _t
-function copy(_t)
+copy = function(_t)
 	local t = {}
 	for k, v in pairs(_t) do
 		t[k] = (type(v) == "table" and copy(v)) or v
 	end
 	return t
-end
+end,
 
 --- Check two tables for equality.
 -- This function checks whether two tables are equal, by checking if the fields are equal
 -- @param t1 The first table to be checked for equality
 -- @param t2 The second table to be checked for equality
 -- @return Whether or not t1 and t2 are equal
-function equal(t1, t2)
+equal = function(t1, t2)
 	if #t1 ~= #t2 then
 		return false
 	end
@@ -33,7 +34,7 @@ function equal(t1, t2)
 		i = i + 1
 	end
 	return eq
-end
+end,
 
 --------------------
 -- Math Functions --
@@ -43,19 +44,20 @@ end
 -- This function takes a number, and rounds it to the nearest integer
 -- @param num The number to be rounded
 -- @return The integer nearest to num
-function round(num)
+round = function(num)
 	return math.floor(num+0.5)
-end
+end,
 
 --- Create a derivative for a function
 -- This function takes two arguments, a function and a delta. The delta can be left unspecified.The function returned is an approximation for the derivative of the given function, and delta specifies the accuracy, where a smaller delta results in a higher accuracy. 
 -- @param f The function to derive from
 -- @param delta The accuracy of the derivation, defaults to 1e-4
 -- @return The derivative of f
-function derive(f, delta)
+derive = function(f, delta)
 	delta = delta or 1e-4
 	return function(x)
 		return ((f(x+delta) - f(x))/delta)
 	end
-end
+end,
+}
 
