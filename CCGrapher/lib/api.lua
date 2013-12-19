9b6b6f56eefeaca21eacb069629130f5ac57c02d
Symmetryc <yuiopqwertyasdfglkjh@hotmail.com>
2013-12-19 18:59:13 -0500

    Changed some stuff

--- CCGrapher/lib/api.lua
+++ CCGrapher/lib/api.lua
@@ -1,6 +1,6 @@
 -----------
 -- API that holds miscellaneous function for CCGrapher
--- @author Symmetric, Wobbo
+-- @author Symmetryc, Wobbo
 -- @module api
 -- @alias M
 
@@ -9,10 +9,10 @@ local M = {}
 --- Create a derivative for a function
 -- This function takes two arguments, a function and a delta. The delta can be left unspecified. The function returned is an approximation for the derivative of the given function, and delta specifies the accuracy, where a smaller delta results in a higher accuracy. 
 -- @tparam func _f The function to derive from
--- @tparam number delta The accuracy of the derivation, defaults to 1e-4
+-- @tparam number delta The accuracy of the derivation, defaults to 1e-6
 -- @treturn func The derivative of _f
 M.derive = function(_f, _delta)
-	local delta = _delta or 1e-4
+	local delta = _delta or 1e-6
 	return function(x)
 		return (_f(x + delta) - _f(x)) / delta
 	end
@@ -31,16 +31,16 @@ M.eq = function(_f)
 end
 
 --- Finds the solution to an equation using Newton-Raphson method
+-- @param api The api itself (call using `:` syntactic sugar)
 -- @param eq The equation that you are solving
--- @param eq_derive The derivative function of eq
--- @param var The variable you are solving for
 -- @param init The initial guess
 -- @param loop How many loops the approximation should go through
 -- @return The solution
-M.nr = function(_eq, _eq_derive, _var, _init, _loop)
-	local ans = _init or 0.1
+M.nr = function(api, _eq, _init, _loop)
+	local ans = _init or 1
+	local fprime = api.derive(_eq)
 	for i = 1, _loop or 5 do
-		ans = ans - _eq(ans) / _eq_derive(_eq, _ans)
+		ans = ans - _eq(ans) / fprime(ans)
 	end
 	return ans
 end
@@ -51,14 +51,16 @@ end
 -- @return The equation solved for x
 -- @return The equation solved for y
 M.impl = function(api, _str)
-	local f = loadstring("return ".._str:gsub("(.-)=(.+)", "(%1)-(%2)"))
+	local f = function(x, y)
+		return setfenv(loadstring("return ".._str:gsub("(.-)=(.+)", "(%1)-(%2)")), setmetatable({x = x, y = y}, {__index = math}))()
+	end
 	local fx = function(x)
-		return api:nr(setfenv(f, {x = x, math = math}), api.derive, "y")
+		return api:nr(function(y) return f(x, y) end)
 	end
 	local fy = function(y)
-		return api:nr(setfenv(f, {y = y, math = math}), api.derive, "x")
+		return api:nr(function(x) return f(x, y) end)
 	end
-	return api:eq(fx), api:eq(fy)
+	return api.eq(fx), api.eq(fy)
 end
 
 return M
